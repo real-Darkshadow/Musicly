799b36baab817df2853129444fa4f97c7038e855
real-Darkshadow <aadarsh.21bcan262@jecrcu.edu.in>
2023-03-06 21:23:58 +0530

    Home fragment screen implemented

---
+++ app/src/main/java/com/example/musicly/LoginUi.kt
@@ -0,0 +1,11 @@
+package com.example.musicly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginUi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_ui)
+    }
+}
---
+++ app/src/main/java/com/example/musicly/home.kt
@@ -0,0 +1,31 @@
+package com.example.musicly
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class home : Fragment() {
+
+    companion object {
+        fun newInstance() = home()
+    }
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
